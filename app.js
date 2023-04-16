205075f31b71fb85b6afb3990c2fcbbb23550f15
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-04-16 12:42:14 +0530

    ..

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 
-
-
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
-  })
+app.post("/", (req, res) => {
+  res.send("POST request to the homepage");
+});
 
-  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const errorHandlers = require("./handlers/errorHandler");
