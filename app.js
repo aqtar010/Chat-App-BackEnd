601b09aefb6ff9b494b5b0a7e0e62e12a468e3b7
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-04 23:00:48 +0530

    recieve data from frontend ans store on db

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
+const connectDB=require('./handlers/db')
+const mongoose = require('mongoose');
+const User = require('./handlers/schemaHandler'); 
+connectDB()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
@@ -16,23 +22,23 @@ app.get("/api", (req, res) => {
 
 app.post("/auth/*", (req, res) => {
   const pass = req.body.Password;
-  const email = req.body.Email;
-  console.log(email, "  ", pass);
+  const email1 = req.body.Email;
+  console.log(email1, "  ", pass);
+  const newUser = new User({
+    email: email1,
+    password: pass,
+  });
+  
+  newUser.save()
+    .then((user) => {
+      console.log('User created:', user);
+    })
+    .catch((error) => {
+      console.error('Error creating user:', error);
+    });
 
   res.json({ message: "Form submission received" });
 });
 
 
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const errorHandlers = require("./handlers/errorHandler");
-app.use(errorHandlers.notFound);
-app.use(errorHandlers.mongoseErrors);
-if (process.env.ENV === "DEVELOPMENT") {
-  app.use(errorHandlers.developmentErrors);
-} else {
-  app.use(errorHandlers.productionErrors);
-}
 module.exports = app;
--- server.js
+++ server.js
@@ -2,34 +2,15 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
-const mongoose = require('mongoose');
-const User = require('./handlers/schemaHandler'); // assuming you have the schema in a separate file
+// assuming you have the schema in a separate file
 
 const cors = require("cors")
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/ChatApp', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+
 
 // create a new user and save it to the database
-const newUser = new User({
-  email: 'user@example.com',
-  password: 'password123',
-});
 
-newUser.save()
-  .then((user) => {
-    console.log('User created:', user);
-  })
-  .catch((error) => {
-    console.error('Error creating user:', error);
-  });
 
 
 app.listen(PORT, () => {
