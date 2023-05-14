70c2f6187354df359eb6b1a7f3e51c130674b315
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-05-15 01:00:16 +0530

    hashing pass

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(cors());
 app.use(bodyParser.json());
-const connectDB=require('./handlers/db')
+const connectDB=require('./handlers/db');
+const bcrypt = require(("bcryptjs"))
 const mongoose = require('mongoose');
 const User = require('./handlers/schemaHandler'); 
 connectDB()
@@ -21,13 +22,13 @@ app.get("/api", (req, res) => {
 });
 
 app.post("/auth/sign-up", (req, res) => {
-  console.log(req.body);
+  
   const {Fname,Lname,Email,Password}=req.body;
   const newUser = new User({
     FirstName:Fname,
     LastName:Lname,
     email: Email,
-    password: Password,
+    password: bcrypt.hashSync(Password, bcrypt.genSaltSync()),
   });
   
   newUser.save()
@@ -39,6 +40,10 @@ app.post("/auth/sign-up", (req, res) => {
     });
 
   res.json({ message: "Form submission received" });
+});
+app.post('/auth/sign-in',async (req,res)=>{
+  const user=await User.findOne({email:req.body.email})
+  user?console.log(user,"User found"):console.log("User Not Found");
 });
 
 
